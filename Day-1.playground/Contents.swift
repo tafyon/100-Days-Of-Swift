aeb183ae1a042bc1fb15b28409f68d6691931b5c
Tamer Afyon <203502050@ogr.altinbas.edu.tr>
2024-08-29 00:26:38 +0300

    Added day 1.

---
+++ Day-1.playground/Contents.swift
@@ -0,0 +1,43 @@
+import UIKit
+
+// Değişkenler - Variables
+var str = "Swift'te 1.Gün "
+var age = 23
+
+print(str)
+print(age)
+
+// Dizeler ve Tam Sayılar - String and Integers
+var population = 8_000_000
+
+// Çok Satırlı Dizeler - Multi-line Strings
+var str1 = """
+This goes over multiple
+lines
+"""
+
+var str2 = """
+This goes \
+over multiple \
+lines
+"""
+
+//Ondalıklı Sayılar ve Boolean'lar - Doubles and Booleans
+var pi = 3.141
+var weight = 67.3
+var likeSwift = true
+
+// Dize Enterpolasyonu - String interpolation
+var score = 85
+var sonuc = "Your score was \(score)"
+print(sonuc)
+
+//Sabitler - Constants
+let taylor = "swift"
+
+//Tür Açıklamaları
+let album: String = "Reputation"
+let year: Int = 1989
+let height: Double = 1.78
+let taylorRocks: Bool = true
+
